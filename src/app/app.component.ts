03c701efd8c0ea993475503d904f429706783f2b
PoyChang <poypost@gmail.com>
2017-03-03 08:42:18 +0800

    加入 Hero 模型並顯示英雄訊息

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
 
+export class Hero {
+  id: number;
+  name: string;
+}
