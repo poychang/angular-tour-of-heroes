3d9b4c0c3dbc59f5a9dbe3c369075f3d85a5bb33
PoyChang <poypost@gmail.com>
2017-03-03 15:39:31 +0800

    將 Hero 服務改成用 Promise 的方式做回傳，來符合未來異步調用資料的情境（例如從資料庫提取資料）

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,6 +29,6 @@ export class AppComponent implements OnInit {
     this.selectedHero = hero;
   }
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().then(result => this.heroes = result);
   }
 }
