54b5c1e20f59a499658acc68391659975610103a
PoyChang <poypost@gmail.com>
2017-03-03 15:24:22 +0800

    將 Hero 服務注入 App 元件，並使用 Hero 服務來提供資料

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 import { Hero } from './models/hero';
 
+import { HeroService } from './hero.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
 export class AppComponent {
+
+  constructor(private heroService: HeroService) { }
+
   title = 'Tour of Heroes';
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
-  heroes = HEROES;
+  heroes = this.heroService.getHeroes();
   selectedHero: Hero;
 
   onSelect(hero: Hero): void {
