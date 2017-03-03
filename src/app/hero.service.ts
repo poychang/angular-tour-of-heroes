e9d34844290b6715768f68fa647fce17cb0c0e19
PoyChang <poypost@gmail.com>
2017-03-03 15:23:39 +0800

    實作 Hero 服務中的 getHeroes 方法

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
 
+import { Hero } from "./models/hero";
+import { HEROES } from './models/mock-heroes';
+
 @Injectable()
 export class HeroService {
 
   constructor() { }
 
+  getHeroes(): Hero[] {
+    return HEROES;
+  };
 }
