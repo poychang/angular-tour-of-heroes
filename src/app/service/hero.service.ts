557cebaec4e34e7a247b4705a737f8aa7fe78547
PoyChang <poypost@gmail.com>
2017-03-06 23:33:48 +0800

    改使用模擬的 Web API 來取得資料，這裡會使用 Http 模組。 這裡的操作開始和 Rx 有這密切關係

--- src/app/service/hero.service.ts
+++ src/app/service/hero.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from "@angular/http";
 
 import { Hero } from "./../models/hero";
 import { HEROES } from './../models/mock-heroes';
 
+import 'rxjs/add/operator/toPromise';
+
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  private herourl = 'api/heroes'; // URL to web api
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
 
   getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+    return this.http.get(this.herourl)
+      .toPromise()
+      .then(response => response.json().data as Hero[])
+      .catch(this.handleError);
   };
   getHero(id: number): Promise<Hero> {
     return this.getHeroes().then(heroes => heroes.find(p => p.id === id));
